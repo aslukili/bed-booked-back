2751dc84561626997e315010c7a2f48323060e53
Abdeslam Loukili <abdeslam.edu@gmail.com>
2023-01-06 11:40:05 +0100

    implement security

--- src/main/java/org/loukili/bedbooker/entity/User.java
+++ src/main/java/org/loukili/bedbooker/entity/User.java
@@ -2,14 +2,26 @@ package org.loukili.bedbooker.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
   @Id
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
   @SequenceGenerator(name = "user_seq")
@@ -57,8 +69,38 @@ public class User {
     this.role = role;
   }
 
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return null;
+  }
+
   public String getPassword() {
     return password;
+  }
+
+  @Override
+  public String getUsername() {
+    return null;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
   }
 
   public void setPassword(String password) {
--- src/main/java/org/loukili/bedbooker/service/ReservationServiceImpl.java
+++ src/main/java/org/loukili/bedbooker/service/ReservationServiceImpl.java
@@ -6,7 +6,9 @@ import org.loukili.bedbooker.repository.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class ReservationServiceImpl implements ReservationService{
@@ -29,9 +31,18 @@ public class ReservationServiceImpl implements ReservationService{
 
   @Override
   public Reservation reserve(Reservation reservation) {
-    reservation.setPrice(1300.0);
     reservation.setUser(userService.getUserById(reservation.getUser().getId()).orElseThrow());
     reservation.setRoom(roomService.getRoomById(reservation.getRoom().getId()).orElseThrow());
+
+    // calculate the total price:
+    reservation.setPrice(getDifferenceDays(reservation.getStartDate(), reservation.getEndDate()) * reservation.getRoom().getPrice());
     return reservationRepository.save(reservation);
+  }
+
+
+  // calculate the days between checkin and checkout days
+  public static long getDifferenceDays(Date d1, Date d2) {
+    long diff = d2.getTime() - d1.getTime();
+    return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
   }
 }
